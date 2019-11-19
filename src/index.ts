9202da39c513a08c31807bf226857a73921edbfb
Billi Ramirez <ramirezbilly42@outlook.com>
2019-11-19 11:36:27 -0600

    json server

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "billi", age: 20 });
-user.on("change", () => {
-  console.log("change # 1");
-});
-user.on("change", () => {
-  console.log("change # 2");
-});
-
-user.on("save", () => {
-  console.log("save was trigger");
-});
-
-user.trigger("change");
-user.trigger("save");
+const user = new User({ name: "alexander", age: 50 });
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from "axios";
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -34,5 +37,22 @@ export class User {
     handlers.forEach(cb => {
       cb();
     });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((res: AxiosResponse): void => {
+        this.set(res.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get("id");
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post("http://localhost:3000/users", this.data);
+    }
   }
 }
