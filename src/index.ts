cb08de7af53b2ee232a51cc421e9e7f02dc01422
Billi Ramirez <ramirezbilly42@outlook.com>
2019-11-27 14:57:03 -0600

    settings accesors to user class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "alexander", age: 50 });
-user.events.on("change", () => {
-  console.log("changing");
+user.on("change", () => {
+  console.log("user was changed");
 });
-
-user.events.trigger("change");
